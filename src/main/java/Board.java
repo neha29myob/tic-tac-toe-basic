09c6771b36a125900196283e237e6031f0f5ef42
Preethi Ramkumar <preethi.ramkumar@myob.com>
2018-04-06 14:15:33 +1000

    Extracted board to an object

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Board {
@@ -8,10 +9,6 @@ public class Board {
         board = new String[size][size];
         initializeBoard(size);
         }
-
-    public String[][] getBoard() {
-        return board;
-    }
 
     private void initializeBoard(int size) {
         for (int row = 0; row < size; row++) {
@@ -25,16 +22,25 @@ public class Board {
         return board.length;
     }
 
-    private List<String> getRow(){
-        List<String> rows = new ArrayList<>();
-        for (int row = 0; row < getBoardSize(); row++) {
-            for (int column = 0; column < getBoardSize(); column++)
-                rows.add((board[row][column]));
-        }
-        return rows;
+//    private List<String> getRow(){
+//        List<String> rows = new ArrayList<>();
+//        for (int row = 0; row < getBoardSize(); row++) {
+//            for (int column = 0; column < getBoardSize(); column++)
+//                rows.add((board[row][column]));
+//        }
+//        return rows;
+//    }
+
+    public void placeMarker(int x, int y, String token) {
+
+        board[x][y] = token;
     }
 
-    private boolean isFull() {
+    public boolean isMoveOutOBounds(int x, int y) {
+        return (x < 0 && x > getBoardSize() && y < 0 && y > getBoardSize());
+    }
+
+    private boolean isDraw() {
         boolean isFull = true;
         for (int row = 0; row < getBoardSize(); row++) {
             for (int column = 0; column < getBoardSize(); column++) {
@@ -46,11 +52,65 @@ public class Board {
         return isFull;
     }
 
-    public void placeMarker(Coordinates coordinates, String token){
-
-        board[coordinates.getX()][coordinates.getY()] = token;
-
+    public boolean isWinningLine(List<String> winningLine) {
+        return ((winningLine.get(0) != "-") && Collections.frequency(winningLine, winningLine.get(0)) == getBoardSize());
     }
+
+    public boolean checkForWin() {
+        return (checkForRowWin() || checkForColumnWin()|| checkForDiagonalWin());
+    }
+
+    public boolean isOver() {
+        return (checkForWin() || isDraw());
+    }
+
+    private boolean checkForRowWin() {
+
+        for (int row = 0; row < getBoardSize(); row++) {
+            List<String> rows = new ArrayList<>();
+            for (int column = 0; column < getBoardSize(); column++)
+                rows.add((board[row][column]));
+
+            if (isWinningLine(rows)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkForColumnWin() {
+        for (int column = 0; column < getBoardSize(); column++) {
+            List<String> columns = new ArrayList<>();
+            for (int row = 0; row < getBoardSize(); row++)
+                columns.add(board[row][column]);
+            if (isWinningLine(columns)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkForDiagonalWin() {
+        List<String> diagonalsLR = new ArrayList<>();
+        List<String> diagonalsRL = new ArrayList<>();
+
+        int boardSize = getBoardSize();
+
+        for (int i = 0; i < boardSize; i++) {
+            diagonalsLR.add(board[i][i]);
+            diagonalsRL.add(board[i][boardSize - i - 1]);
+        }
+
+        if (isWinningLine(diagonalsLR)) {
+            return true;
+        }
+        if (isWinningLine(diagonalsRL)) {
+            return true;
+        }
+        return false;
+    }
+
+
 
 
 
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,4 +1,3 @@
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,110 +17,79 @@ public class GameTest {
       assertFalse(game.isOver());
     }
 
-    @Test
-    public void whenPlayer1EnterCoordinatesThenPlaceXonBoard(){
-      String[][] expected = {{"x","-","-"},{"-","-","-"},{"-","-","-"}};
-      game.placeMarker(0,0);
-      Assert.assertArrayEquals(expected,game.getBoard());
-      }
-
-    @Test
-    public void whenPlayer2EnterCoordinatesThenPlaceOonBoard(){
-        String[][] expected = {{"x","-","-"},{"o","-","-"},{"-","x","-"}};
-        game.placeMarker(0,0);
-        game.placeMarker(1,0);
-        game.placeMarker(2, 1);
-        Assert.assertArrayEquals(expected,game.getBoard());
-
-    }
 
     @Test
     public void whenPlayerCapturedARowReturnWinner(){
-        String[][] expected = {{"-","-","-"},
-                               {"o","-","o"},
-                               {"x","x","x"}};
-        game.placeMarker(2,0);
-        game.placeMarker(1,0);
-        game.placeMarker(2,1);
-        game.placeMarker(1,2);
-        game.placeMarker(2,2);
-        //Assert.assertArrayEquals(expected, game.getBoard());
-        assertTrue(game.checkForWin());
+        game.play(2,0);
+        game.play(1,0);
+        game.play(2,1);
+        game.play(1,2);
+        game.play(2,2);
+        assertTrue(game.isOver());
     }
 
     @Test
     public void whenPlayerCapturedAColumnReturnWinner(){
-        String[][] expected = {{"x","o","x"},
-                               {"-","o","x"},
-                               {"-","o","-"}};
-        game.placeMarker(0,0);
-        game.placeMarker(0,1);
-        game.placeMarker(0,2);
-        game.placeMarker(1,1);
-        game.placeMarker(1,2);
-        game.placeMarker(2,1);
-        //Assert.assertArrayEquals(expected, game.getBoard());
-       assertTrue(game.checkForWin());
+
+        game.play(0,0);
+        game.play(0,1);
+        game.play(0,2);
+        game.play(1,1);
+        game.play(1,2);
+        game.play(2,1);
+       assertTrue(game.isOver());
     }
 
     @Test
     public void whenPlayerCapturedADiagonalLeftToRightReturnWinner(){
-        String[][] expected = {{"x","o","-"},
-                               {"-","x","-"},
-                               {"-","o","x"}};
-        game.placeMarker(0,0);
-        game.placeMarker(0,1);
-        game.placeMarker(1,1);
-        game.placeMarker(2,1);
-        game.placeMarker(2,2);
-        Assert.assertArrayEquals(expected, game.getBoard());
-       assertTrue(game.checkForWin());
+        game.play(0,0);
+        game.play(0,1);
+        game.play(1,1);
+        game.play(2,1);
+        game.play(2,2);
+       assertTrue(game.isOver());
     }
 
     @Test
     public void whenPlayerCapturedADiagonalRightToLeftReturnWinner(){
-        String[][] expected = {{"-","o","x"},
-                               {"-","x","-"},
-                               {"x","o","-"}};
-        game.placeMarker(0,2);
-        game.placeMarker(0,1);
-        game.placeMarker(1,1);
-        game.placeMarker(2,1);
-        game.placeMarker(2,0);
-        Assert.assertArrayEquals(expected, game.getBoard());
-       assertTrue(game.checkForWin());
+        game.play(0,2);
+        game.play(0,1);
+        game.play(1,1);
+        game.play(2,1);
+        game.play(2,0);
+       assertTrue(game.isOver());
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowExceptionWhenMarkerIsPlacedAfterTheGameIsWon(){
-        game.placeMarker(0,0);
-        game.placeMarker(1,0);
-        game.placeMarker(0,1);
-        game.placeMarker(1,2);
-        game.placeMarker(0,2); //player already won here
-        game.placeMarker(2,0);
+        game.play(0,0);
+        game.play(1,0);
+        game.play(0,1);
+        game.play(1,2);
+        game.play(0,2); //player already won here
+        game.play(2,0);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowExceptionWhenMarkerIsPlacedAfterTheGameIsDraw(){
-        game.placeMarker(0,0);
-        game.placeMarker(0,1);
-        game.placeMarker(0,2);
-        game.placeMarker(1,0);
-        game.placeMarker(1,2);
-        game.placeMarker(1,1);
-        game.placeMarker(2,1);
-        game.placeMarker(2,2);
-        game.placeMarker(2,0); // Match is draw here
-        game.placeMarker(2,0);
+        game.play(0,0);
+        game.play(0,1);
+        game.play(0,2);
+        game.play(1,0);
+        game.play(1,2);
+        game.play(1,1);
+        game.play(2,1);
+        game.play(2,2);
+        game.play(2,0); // Match is draw here
+        game.play(2,0);
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void shouldThrowExceptionWhenMarkerIsPlacedOutOfBoard(){
-        game.placeMarker(0,0);
-        game.placeMarker(1,0);
-        game.placeMarker(0,1);
-        game.placeMarker(8,2);
+        game.play(0,0);
+        game.play(1,0);
+        game.play(0,1);
+        game.play(8,2);
 
     }
 
