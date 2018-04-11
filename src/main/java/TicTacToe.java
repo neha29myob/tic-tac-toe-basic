f99b4f031db3f81ba6dfd2288f9550945e3dd04d
Kalyani Singh <kalyani.singh@myob.com>
2018-04-11 17:18:53 +1000

    added quit game

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -5,6 +5,7 @@ public class TicTacToe {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
+        //String input = scanner.nextLine();
         System.out.println("Welcome to Tic Tac Toe");
         System.out.println("Enter the size of board");
         int size = Integer.parseInt(scanner.nextLine());
@@ -18,12 +19,17 @@ public class TicTacToe {
         while(game.getStatus().equals(GameState.PLAYING)){
 
             System.out.printf("Player %d enter a coord x,y to place your %s or enter 'q' to give up:", game.getOrderOfCurrentPlayer(), game.getTokenOfCurrentPlayer());
+            String userChoice = scanner.nextLine();
+            if(userChoice.equalsIgnoreCase("q")){
+                System.out.println("The game is exiting");
+                System.exit(0);
+            }
 
-            String coordinates = scanner.nextLine();
-            Coordinates input = Coordinates.convertInput(coordinates);
+            //String coordinates = input;
+            Coordinates coordinates = Coordinates.convertInput(userChoice);
 
-            if(game.isValid(input)) {
-                game.play(input);
+            if(game.isValid(coordinates)) {
+                game.play(coordinates);
                 if (game.getStatus().equals(GameState.WIN)) {
                     System.out.println("Move accepted, well done you've won the game!");
                 } else {
